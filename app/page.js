409bb7b8ae22481effca54d699d980518a7aa4ed
Lou-Ann BARRY <148325055+ghostofus@users.noreply.github.com>
2026-03-27 17:32:00 +0100

    test pour ajout d'un autre projet

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ const PROJECTS = [
     { title: "IRA" , period: "2025/2026 : 10 mois", role: "Chef de projet", summary: "Souls like narratif 3D réaliste - Unreal Engine 5", tags: ["Agile", "JIRA", "Roadmapping", "Risk mgmt"], link: "#about", src: "/ira.png" },
     { title: "Gecko Pulco" , period: "2025 : 4 mois ", role: "Chef de projet", summary: "Plateformer 3D cartoon - Unity 6", tags: ["Scrum", "Stakeholders", "KPIs"], link: "#about", src: "/gecko.jpg"  },
     { title: "Figure out" , period: "2023/2024", role: "Directrice Artistique", summary: "Click & Point 3D - Unreal", tags: ["Planning", "Delivery", "QA"], link: "#about", src: "/figureout.jpg"  },
+    { title: "Figure out" , period: "2023/2024", role: "Directrice Artistique", summary: "Click & Point 3D - Unreal", tags: ["Planning", "Delivery", "QA"], link: "#about", src: "/figureout.jpg"  },
 ];
 
 const EXPERIENCE = [
